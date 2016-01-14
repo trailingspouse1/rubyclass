8f47f314c30533b31dee800d89abd156eb225ecb
Diana Mahmoud <dianamahmoud@mac.com>
2016-01-14 11:07:29 -0500

    first push

---
+++ dogsayscatsays.rb
@@ -0,0 +1,13 @@
+puts 'Choose cat or dog'
+ response = gets.chomp.downcase
+if response == 'dog'
+  puts 'bark'
+elsif response =='cat'
+  puts 'meow' 
+else
+  puts 'Sorry try again!'	
+end
+
+
+
+
