6c8534c7c199fc278c2fd64bf125845e2ee6ca2d
Diana Mahmoud <dianamahmoud@mac.com>
2016-01-20 08:55:45 -0500

    first push of homework

--- timeofday.rb
+++ timeofday.rb
@@ -1,1 +1,15 @@
 #Write a program that asks for the time of day and then greets a user appropiately. Try using case statements.
+
+ puts 'What time is it? USE MILITARY TIME.'
+ answer = gets.chomp.to_i
+ case answer
+ when 0..1200
+ 	puts 'Good morning'
+ when 1300..1600
+ 	puts 'Good Afternoon!'
+ else
+ 	puts 'Good night!'
+ end
+
+
+
